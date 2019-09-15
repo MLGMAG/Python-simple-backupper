a29485a99014316ea92635b1481a6e189745c52c
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2019-09-15 23:43:36 +0300

    Added colorful output)

--- file_backup.py
+++ file_backup.py
@@ -11,6 +11,15 @@ file_folder_path = current_path + os.sep + files_folder
 backup_folder_path = current_path + os.sep + backup_folder
 
 
+def print_info(text): print('\033[93m{}\033[00m'.format(text))
+
+
+def print_success(text): print('\033[92m{}\033[00m'.format(text))
+
+
+def print_warning(text): print('\033[91m {}\033[00m'.format(text))
+
+
 def get_folders_from_dir(path):
     objects_in_folder = os.listdir(path)
     folders = os.listdir(path)
@@ -23,22 +32,19 @@ def get_folders_from_dir(path):
     return folders
 
 
-# We need to check folder of those file containing
-
-
 def on_startup():
     print()
     if files_folder not in get_folders_from_dir(current_path):
         os.mkdir(files_folder + os.sep)
-        print('[Logger]: File ("{}") isn\'t exist, I create one'.format(files_folder))
+        print_info('[Logger]: File ("{}") isn\'t exist, I create one'.format(files_folder))
     else:
-        print('[Logger]: File ("{}") is exist'.format(files_folder))
+        print_info('[Logger]: File ("{}") is exist'.format(files_folder))
 
     if backup_folder not in get_folders_from_dir(current_path):
         os.mkdir(backup_folder + os.sep)
-        print('[Logger]: File ("{}") isn\'t exist, I create one'.format(backup_folder))
+        print_info('[Logger]: File ("{}") isn\'t exist, I create one'.format(backup_folder))
     else:
-        print('[Logger]: File ("{}") is exist'.format(backup_folder))
+        print_info('[Logger]: File ("{}") is exist'.format(backup_folder))
     print()
     console_helper()
 
@@ -46,7 +52,7 @@ def on_startup():
 def console_helper():
     time.sleep(1)
     print('=========================================================================================')
-    print('Hello, it\'s Backup Manager v.1.0.0')
+    print_success('Hello, it\'s Backup Manager v.1.0.0')
     print('=========================================================================================')
     time.sleep(2)
     print('I can backup yours "important" files);\n')
@@ -61,7 +67,7 @@ def console_helper():
 
 def backup_files():
     time.sleep(1)
-    print('[Logger]: Now I\'am in ({}) folder'.format(file_folder_path))
+    print_info('[Logger]: Now I\'am in ({}) folder'.format(file_folder_path))
     files_path = []
     files_in_folder = []
     for folder_path, folders_in_path, files in os.walk(file_folder_path):
@@ -70,34 +76,35 @@ def backup_files():
                 files_path.append(folder_path + os.sep + file)
                 files_in_folder.append(file)
             else:
-                print('File: "{}" is to large (Max is 1MB);'.format(file))
+                print_warning('File: "{}" is to large (Max is 1MB);'.format(file))
     count_files_in_folder = len(files_in_folder)
 
     if count_files_in_folder == 0:
         time.sleep(1)
-        print('[Logger]: No files to backup\n')
+        print_warning('[Logger]: No files to backup\n')
     elif count_files_in_folder >= 200:
         time.sleep(1)
-        print('[Logger]: It\'s to many files: {}\n'.format(count_files_in_folder))
+        print_warning('[Logger]: It\'s to many files: {}\n'.format(count_files_in_folder))
     else:
         zip_file_name = (datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.zip')
-        print('[Logger]: Here it is {} files'.format(count_files_in_folder))
+        print_info('[Logger]: Here it is {} files'.format(count_files_in_folder))
         time.sleep(1)
-        print('[Logger]: Files in folder:', files_in_folder)
+        print('Files in folder:', files_in_folder)
         time.sleep(1)
-        print('[Logger]: Start creating of backup and backing up all files')
+        print_info('[Logger]: Start creating of backup and backing up all files')
         backup = ZipFile(zip_file_name, 'w')
         for file in files_path:
             backup.write(file)
         backup.close()
         time.sleep(2)
-        print('[Logger]: All files successfully backed up')
+        print_info('[Logger]: All files successfully backed up')
         time.sleep(1)
-        print('[Logger]: Moving "{}" to ("{}") folder'.format(zip_file_name, backup_folder))
+        print_info('[Logger]: Moving "{}" to ("{}") folder'.format(zip_file_name, backup_folder))
         shutil.move(current_path + os.sep + zip_file_name, backup_folder)
         time.sleep(1.5)
-        print('Backup successfully completed!')
-        print('You can find your backup in folder ("{}") with name "{}";\n'.format(backup_folder, zip_file_name))
+        print_success('Backup successfully completed!')
+        print_success(
+            'You can find your backup in folder ("{}") with name "{}";\n'.format(backup_folder, zip_file_name))
         time.sleep(1)
 
 
